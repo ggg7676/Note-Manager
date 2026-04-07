aad6c97e0966dfff6632c7e228fa003c4241cf73
ggg7676 <48100611+ggg7676@users.noreply.github.com>
2026-04-07 19:40:14 +0300

    Add an additional check

--- commands.py
+++ commands.py
@@ -1,98 +1,64 @@
-def get_last_id():
-    try:
-        with open("notesdb.txt", "r") as notes_file:
-            all_notes = notes_file.read().strip().split("\n\n")
-    except FileNotFoundError:
-        return 0
+import shlex
+from commands import add_note, show_note, list_notes, delete_note, help, ID
+
+def main():
     
-    all_notes = [note for note in all_notes if note.strip()]
-    
-    if not all_notes:
-        return 0
-    else:
-        return int(all_notes[-1].splitlines()[0])
+    while True:
+        print("Enter a command and press enter twice: ")
+        lines = []
 
-ID = get_last_id()
+        while True:
+            line = input()
 
-def add_note(title, content):
-    
-    with open("notesdb.txt", "a") as notes_file:
-        global ID
-        ID += 1
-        notes_file.write(str(ID) + "\n")
-        notes_file.write(title + "\n")
-        notes_file.write(content + "\n")
-        notes_file.write("\n")
+            if line == 'q':
+                return
+            
+            if line == "":
+                if not lines: 
+                    print("Invalid command.\n")
+                    print("Enter a command and press enter twice: ")
+                    continue
+                else:
+                    break
 
-    return "Note added successfully.\n"
+            lines.append(line)
 
+        if len(lines) == 1:
 
-def list_notes():
-    try:
-        with open("notesdb.txt", "r") as notes_file:
-            all_notes = notes_file.read()
-    except FileNotFoundError:
-        return "File not found.\n"
-    
-    return all_notes
+            command_line = lines[0]
 
+            split_input = shlex.split(command_line)
+            command_type = split_input[1]
 
-def show_note(id):
-    try:
-        with open("notesdb.txt", "r") as notes_file:
-            all_content = notes_file.read()
-    except FileNotFoundError:
-        return "File not found.\n"
+            if command_type == 'add':
+                print(add_note(split_input[2], split_input[3]))
 
-    if not all_content:
-        return "Empty file.\n"
+            elif command_type == 'list':
+                print(list_notes())
 
-    all_notes = all_content.strip().split("\n\n")
+            elif command_type== 'show':
+                print(show_note(split_input[2]))
 
-    for note in all_notes:
-        if str(id) == note.splitlines()[0]:
-            return note + "\n"
-        
-    return "Invalid note id.\n"
+            elif command_type == 'delete':
+                print(delete_note(split_input[2]))
+                
+            elif command_type == '--help':
+                help()
+            
+            else:
+                print("Invalid command.")
 
-
-def delete_note(id):
-    try:
-        with open("notesdb.txt", "r") as notes_file:
-            all_content = notes_file.read()
-    except FileNotFoundError:
-        return "File not found.\n"
-    
-    all_notes = all_content.strip().split("\n\n")
-
-    notes_to_copy = []
-    found = False
-
-    for note in all_notes:
-        if str(id) != note.splitlines()[0]:
-            notes_to_copy.append(note)
         else:
-            found = True
+            full_command = ""
+            for line in lines:
+                full_command += line + "\n"
+            
+            full_command = full_command.split('"')
+            print(add_note(full_command[1], full_command[2]))
 
    
-    with open("notesdb.txt", "w") as notes_file:
-        for curr_note in notes_to_copy:
-            notes_file.write(curr_note)
-            notes_file.write("\n\n")
-   
 
-
-    if found:
-        return "Note deleted successfully.\n"
-    else:
-        return "Invalid note id.\n"
-
-
-def help():
-    print("Available commands (press enter twice to execute):")
-    print("note add \"title\" \"content\" -> to add a new note")
-    print("note list -> to view all notes")
-    print("note show 'id' -> to view note with specified id")
-    print("note delete 'id' -> to delete a note with specified id")
-    print("notes --help -> to view commands")
-    print("q -> to quit\n")
+if __name__ == "__main__":
+    main()
+    
+    
